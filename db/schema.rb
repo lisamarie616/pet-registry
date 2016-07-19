23e3762a104c23e2e689ee651e1687582cc6e494
lisamarie616 <lisamarie616@gmail.com>
2016-07-19 16:16:15 -0700

    change gender field to dropdown and add username to user

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20160719215123) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "username"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
