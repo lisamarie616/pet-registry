633b145e88dacbea18b8f8d56bfa335d9e8b6afe
lisamarie616 <lisamarie616@gmail.com>
2016-07-19 16:02:29 -0700

    add formatting to form and data before save

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -3,5 +3,12 @@ class Animal < ActiveRecord::Base
 
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "150x150>" }, default_url: ":style/default.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
-  validates :name, :type, :sex, presence: true
+  validates :name, :animal_type, :sex, presence: true
+  before_save :format_data
+
+  protected
+  def format_data
+    self.name = self.name.titlecase
+    self.animal_type = self.animal_type.titlecase
+  end
 end
