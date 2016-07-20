b112583c319dec7f179047b5735f2657864d5495
lisamarie616 <lisamarie616@gmail.com>
2016-07-19 18:37:09 -0700

    update README and add data to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Animal.create([
+  { name: 'Lassie', animal_type: 'Dog', sex: 'Female', user_id: 1 }, 
+  { name: 'Fido', animal_type: 'Dog', sex: 'Male', user_id: 1 }, 
+  { name: 'Snoopy', animal_type: 'Dog', sex: 'Male', user_id: 1 }, 
+  { name: 'Garfield', animal_type: 'Cat', sex: 'Male', user_id: 1 }, 
+  { name: 'Scotty', animal_type: 'Dog', sex: 'Male', user_id: 1 }, 
+  { name: 'Teddy', animal_type: 'Dog', sex: 'Male', user_id: 1 }, 
+  { name: 'Fluffy', animal_type: 'Fish', sex: 'Female', user_id: 1 }, 
+  { name: 'Grumpy Cat', animal_type: 'Cat', sex: 'Female', user_id: 1 }, 
+  { name: 'Lil Bub', animal_type: 'Cat', sex: 'Female', user_id: 1 }, 
+  { name: 'Lucky', animal_type: 'Dog', sex: 'Male', user_id: 1 }, 
+])
